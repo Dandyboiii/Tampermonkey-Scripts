afae8bd644522a0275e53f1cd7523fc662b26200
Dandyboi <70469187+Dandyboiii@users.noreply.github.com>
2022-08-08 18:20:41 -0400

    Update Instagram-Dark-Mode.js

--- Scripts/Instagram-Dark-Mode.js
+++ Scripts/Instagram-Dark-Mode.js
@@ -1,1 +1,23 @@
-<--! this is a placeholder -->
+// ==UserScript==
+// @name        Instagram DarkMode
+// @description Automatically input
+// @updateURL   https://github.com/Dandyboiii/Tampermonkey-Scripts/blob/main/Scripts/Instagram-Dark-Mode.js
+// @downloadURL https://github.com/Dandyboiii/Tampermonkey-Scripts/blob/main/Scripts/Instagram-Dark-Mode.js
+// @version     1.0.0
+// @match       *://*.instagram.com/*
+// @run-at      document-start
+// @grant       none
+// ==/UserScript==
+
+var oldUrlSearch = window.location.search;
+
+if ( ! /\?theme=dark$/.test (oldUrlSearch) ) {
+
+    var newURL = window.location.protocol + "//"
+               + window.location.host
+               + window.location.pathname
+               + oldUrlSearch + "?theme=dark"
+               + window.location.hash
+               ;
+    window.location.replace (newURL);
+}
